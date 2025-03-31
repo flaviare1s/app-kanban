563126a1b8f071652fd3b364ff4c10c9daf1bf96
flaviare1s <flaviarster@gmail.com>
2025-03-31 08:51:49 -0300

    Instala e configura Jest e React Testing Library

---
+++ babel.config.cjs
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+    "@babel/preset-react",
+    {
+      runtime: "automatic",
+    },
+    "@babel/preset-typescript",
+  ],
+};
